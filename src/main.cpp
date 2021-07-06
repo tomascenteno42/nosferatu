20ef1ecc26168c7bca9aeace472b642a14e97324
tomascenteno42 <centenomas253@gmail.com>
2021-07-06 19:08:16 -0300

    Se emprolijo la estructura de archivos

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "../lib/interfaz/Menu.h"
+
+using namespace std;
+
+int main()
+{
+    Menu *juego = new Menu(PATH_ARCHIVO_ESTADO); // PATH EN CONSTANTES
+    bool seguir_jugando = true;
+    while (seguir_jugando)
+    {
+        juego->mostrar_opciones();
+        juego->pedir_opcion();
+        juego->procesar_opcion();
+        seguir_jugando = juego->obtener_opcion_elegida() != 0;
+    }
+    delete juego;
+    return 0;
+}
